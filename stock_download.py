a64fef53938eae5d918e562a4c7fbe40b87608bf
Zui <38757448+Duy-Vu@users.noreply.github.com>
2022-03-09 12:06:25 +0200

    Update stock_download.py
    - Remove UNIX datetime
    - Sort list of symbols
    - Try to download stock until querying end date before download from max interval
    - Make sure index of dataframe is "Date"

--- stock_download.py
+++ stock_download.py
@@ -1,36 +1,36 @@
 import pandas_datareader.data as web
 import pandas as pd
 import tqdm
-import datetime
 
-start_time = "23 Mar, 2020 13:00:05 +0000"
-end_time = "03 Jan, 2022 13:00:05 +0000"
-
-interval = [start_time, end_time]
-
-interval_object = [datetime.datetime.strptime(date, "%d %b, %Y %H:%M:%S %z") for date in interval]
-interval_timestamp = [int(datetime.datetime.timestamp(date_object)) for date_object in interval_object]
-
-# def get_close(symbol):
-#     print(web.DataReader(symbol, 'yahoo', "23 Mar, 2020 13:00:05 +0000", "25 Mar, 2020 13:00:05 +0000")["Adj Close"].values)
-#     return web.DataReader(symbol, 'yahoo', "23 Mar, 2020 13:00:05 +0000", "25 Mar, 2020 13:00:05 +0000")["Adj Close"].values
+start_day = "2020-03-23"
+end_day = "2022-01-03"
 
 # The first table on the Wikipedia page is the S&P 500 component stocks.
 # The second one is the selected changes to the list of S&P 500 components.
 wiki_link = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
 
 sp_500_table = pd.read_html(wiki_link)[0]
-sp_500_symbols = sp_500_table['Symbol'].str.replace('.', '-', regex=False).values.tolist()
-sp_500_adj_close= {}
-for symbol in tqdm.tqdm(iterable=sp_500_symbols, desc="Get stock done", total=len(sp_500_symbols), unit="stock"):
+sp_500_symbols = sorted(sp_500_table['Symbol'].str.replace('.', '-', regex=False).tolist())
+print("All S&P 500 stocks:\n", sp_500_symbols, "\n")
+
+sp_500_adj_close = {}
+for symbol in tqdm.tqdm(iterable=sp_500_symbols, desc="Downloaded stocks", total=len(sp_500_symbols), unit="stock"):
     try:
-        sp_500_adj_close[symbol] = web.DataReader(symbol, 'yahoo', start="2020-03-23", end="2022-01-03", retry_count=5, pause=0.2)["Adj Close"]
-    except:
+        sp_500_adj_close[symbol] = web.DataReader(symbol, 'yahoo', start=start_day, end=end_day, retry_count=5, pause=0.2)["Adj Close"]
+    except KeyError:
         print(symbol)
-        sp_500_adj_close[symbol] = web.DataReader(symbol, 'yahoo', retry_count=5, pause=0.2)["Adj Close"]
+        try:
+            print("First added date of stock", symbol, "is after the querying start date.", end='')
+            sp_500_adj_close[symbol] = web.DataReader(symbol, 'yahoo', end=end_day, retry_count=5, pause=0.2)["Adj Close"]
+            print(" Stock until querying end date:\n", sp_500_adj_close[symbol], "\n")
+        except KeyError:
+            print("..and also the end date. Hence, ignore!\n")
 
-df = pd.DataFrame(sp_500_adj_close)
-print(df)
-df.to_csv('S&P500-info.csv')
+sp_500_adj_close_df = pd.DataFrame(sp_500_adj_close)
+if sp_500_adj_close_df.index.name != "Date":
+    sp_500_adj_close_df.set_index("Date", inplace=True)
 
-# df.to_csv('S&P500-Info.csv')
+unadded_stock = set(sp_500_symbols) - set(list(sp_500_adj_close_df.columns))
+print("Unadded stocks:", unadded_stock)
+
+sp_500_adj_close_df.to_csv("sp500-info.csv")
