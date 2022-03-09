6afb63965e6447b4367570b11247c63dbd97938b
duy <1205vuduy2001@gmail.com>
2022-03-09 23:07:33 +0200

    new script to download sp400 and add file to gitignore

--- stock_download.py
+++ stock_download.py
@@ -2,40 +2,38 @@ import pandas_datareader.data as web
 import pandas as pd
 import tqdm
 
-def get_sp500_close(start_day, end_day, result_csv_dir):
-    # The first table on the Wikipedia page is the S&P 500 component stocks.
-    # The second one is the selected changes to the list of S&P 500 components.
-    wiki_link = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+def get_stock_close_price(source, col_name, start_day, end_day, result_csv_dir):
+    stock_table = pd.read_html(source)[0]
+    stock_symbols = sorted(stock_table[col_name].str.replace('.', '-', regex=False).tolist())
+    print("Stock symbols:")
+    print(stock_symbols, "\n")
 
-    sp_500_table = pd.read_html(wiki_link)[0]
-    sp_500_symbols = sorted(sp_500_table['Symbol'].str.replace('.', '-', regex=False).tolist())
-    print("S&P 500 stock symbols:")
-    print(sp_500_symbols, "\n")
-
-    sp_500_adj_close = {}
-    for symbol in tqdm.tqdm(iterable=sp_500_symbols, desc="Downloaded stocks", total=len(sp_500_symbols), unit="stock"):
+    stock_adj_close = {}
+    for symbol in tqdm.tqdm(iterable=stock_symbols, desc="Downloaded stocks", total=len(stock_symbols), unit="stock"):
         try:
-            sp_500_adj_close[symbol] = web.DataReader(symbol, 'yahoo', start=start_day, end=end_day, retry_count=5, pause=0.2)["Adj Close"]
+            stock_adj_close[symbol] = web.DataReader(symbol, 'yahoo', start=start_day, end=end_day, retry_count=5, pause=0.2)["Adj Close"]
         except KeyError:
             try:
                 print("\nFirst added date of stock", symbol, "is after the querying start date.", end='')
-                sp_500_adj_close[symbol] = web.DataReader(symbol, 'yahoo', end=end_day, retry_count=5, pause=0.2)["Adj Close"]
-                print(" Stock until querying end date:\n", sp_500_adj_close[symbol], "\n")
+                stock_adj_close[symbol] = web.DataReader(symbol, 'yahoo', end=end_day, retry_count=5, pause=0.2)["Adj Close"]
+                print(" Stock until querying end date:\n", stock_adj_close[symbol], "\n")
             except KeyError:
                 print("..and end date. Hence, ignore!\n")
 
-    sp_500_adj_close_df = pd.DataFrame(sp_500_adj_close)
-    if sp_500_adj_close_df.index.name != "Date":
-        sp_500_adj_close_df.set_index("Date", inplace=True)
+    stock_adj_close_df = pd.DataFrame(stock_adj_close)
+    if stock_adj_close_df.index.name != "Date":
+        stock_adj_close_df.set_index("Date", inplace=True)
 
-    unadded_stock = set(sp_500_symbols) - set(list(sp_500_adj_close_df.columns))
+    unadded_stock = set(stock_symbols) - set(list(stock_adj_close_df.columns))
     print("Unadded stocks:", unadded_stock)
 
-    sp_500_adj_close_df.to_csv(result_csv_dir)
-    return sp_500_adj_close_df
+    stock_adj_close_df.to_csv(result_csv_dir)
+    return stock_adj_close_df
 
 if __name__ == "__main__":
     start_day = "23 Mar, 2020"
     end_day = "03 Jan, 2022"
-    download_data_name = "sp500-info.csv"
-    sp_500_df = get_sp500_close(start_day, end_day, download_data_name)
+    source = "https://en.wikipedia.org/wiki/List_of_S%26P_400_companies" # "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+    col_name = "Ticker symbol" # "Symbol"
+    download_data_name = "sp400-info.csv"
+    stock_df = get_stock_close_price(source, col_name, start_day, end_day, download_data_name)
