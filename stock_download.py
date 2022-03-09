6f59b4dc32a8a1b00834405c91a149ee8b4ce032
Zui <38757448+Duy-Vu@users.noreply.github.com>
2022-03-09 12:43:07 +0200

    Using clearer date time format
    Month name is used instead of number.

--- stock_download.py
+++ stock_download.py
@@ -2,8 +2,8 @@ import pandas_datareader.data as web
 import pandas as pd
 import tqdm
 
-start_day = "2020-03-23"
-end_day = "2022-01-03"
+start_day = "23 Mar, 2020"
+end_day = "03 Jan, 2022"
 
 # The first table on the Wikipedia page is the S&P 500 component stocks.
 # The second one is the selected changes to the list of S&P 500 components.
@@ -11,20 +11,20 @@ wiki_link = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
 
 sp_500_table = pd.read_html(wiki_link)[0]
 sp_500_symbols = sorted(sp_500_table['Symbol'].str.replace('.', '-', regex=False).tolist())
-print("All S&P 500 stocks:\n", sp_500_symbols, "\n")
+print("S&P 500 stock symbols:")
+print(sp_500_symbols, "\n")
 
 sp_500_adj_close = {}
 for symbol in tqdm.tqdm(iterable=sp_500_symbols, desc="Downloaded stocks", total=len(sp_500_symbols), unit="stock"):
     try:
         sp_500_adj_close[symbol] = web.DataReader(symbol, 'yahoo', start=start_day, end=end_day, retry_count=5, pause=0.2)["Adj Close"]
     except KeyError:
-        print(symbol)
         try:
-            print("First added date of stock", symbol, "is after the querying start date.", end='')
+            print("\nFirst added date of stock", symbol, "is after the querying start date.", end='')
             sp_500_adj_close[symbol] = web.DataReader(symbol, 'yahoo', end=end_day, retry_count=5, pause=0.2)["Adj Close"]
             print(" Stock until querying end date:\n", sp_500_adj_close[symbol], "\n")
         except KeyError:
-            print("..and also the end date. Hence, ignore!\n")
+            print("..and end date. Hence, ignore!\n")
 
 sp_500_adj_close_df = pd.DataFrame(sp_500_adj_close)
 if sp_500_adj_close_df.index.name != "Date":
